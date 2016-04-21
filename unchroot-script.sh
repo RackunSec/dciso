c9b392d80cb97f1c2a813943af1bb3c013843325
Douglas Berdeaux <weaknetlabs@gmail.com>
2016-04-21 08:13:30 -0400

    Renamed

--- unchroot-script.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#
-# Run this AFTER leavin gthe chrooted environment
-#  to unmount stuff
-echo "Umounting devices in chroot/"
-umount {chroot/dev/pts,chroot/dev,chroot/sys,chroot/proc}
