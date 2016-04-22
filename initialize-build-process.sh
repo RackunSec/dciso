9a2bcdef12f32135215e4f630304ead938ef1b10
Douglas Berdeaux <weaknetlabs@gmail.com>
2016-04-22 09:38:22 -0400

    Added check for initialization script

--- initialize-build-process.sh
+++ initialize-build-process.sh
@@ -2,6 +2,21 @@
 #
 # This script will setup the chrooted env to begin customization and should ONLY be
 #  ran once at the beginning of the entire development process
-lb clean # cleans out ./chroot
-lb config
-lb build
+if [ -d "./chroot" ]; then
+ printf "WARNING!! directory \"chroot\" found.\nIf you continue, all work in \"chroot\" will be LOST.\n\n";
+ printf "Continue (y/n)? ";
+ read ans;
+ if [ "$ans" = "y" ]; then
+  rm -rf chroot
+  lb clean # cleans out ./chroot
+  lb config
+  lb build
+ else
+  printf "Phew! That was a close one!\n";
+ fi
+else
+ printf "Creating \"chroot\" workspace\n";
+ lb clean # cleans out ./chroot
+ lb config
+ lb build
+fi
