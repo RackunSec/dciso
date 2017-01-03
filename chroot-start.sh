74998a9fac112ec9fa65996ba1967bdc13c699ff
Douglas Berdeaux <weaknetlabs@gmail.com>
2017-01-03 13:08:19 -0500

    Updated for x11
    updated for those who need to use X in the chrooted environment!

--- chroot-start.sh
+++ chroot-start.sh
@@ -2,8 +2,11 @@
 # WT7- Building the ISO requires a lot of leg work
 #  so I am scripting it.
 #
-echo "Mounting devices into chroot"
-mount --bind /dev  chroot/dev/
-mount --bind /dev/pts chroot/dev/pts/
-mount --bind /proc chroot/proc/
-mount --bind /sys  chroot/sys/
+echo "Mounting devices into chroot, when done, you will be chrooted... "
+mount --bind /dev ./chroot/dev/
+mount --bind /dev/pts ./chroot/dev/pts/
+mount --bind /dev/shm ./chroot/dev/shm # REQUIRED
+mount --bind /proc ./chroot/proc/
+mount --bind /sys  ./chroot/sys/
+mount --bind /run ./chroot/run # for x11 mouse and keyboard functioning!
+chroot ./chroot
