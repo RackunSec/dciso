8ebe33e4321dbbaea10bbd1042823bea15e2d50c
Douglas Berdeaux <weaknetlabs@gmail.com>
2018-12-21 12:34:09 -0500

    Create fs-sync.sh

---
+++ fs-sync.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# This is part of the new development process and should only be used for Debian 9+
+# New for 2019 Debian development
+# (C) GNU - WEAKNETLABS 2019, DEMON LINUX, Douglas Berdeaux
+WORKINGDIR= # This needs to be set in the root, so e.g: "/debianlive"
+rsync -a / $WORKINGDIR/debian-custom-iso-scripts/chroot --exclude=/{$WORKINGDIR,lib/live,live,cdrom,mnt,proc,run,sys,media,boot}
