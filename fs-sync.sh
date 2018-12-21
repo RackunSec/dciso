9cee6eab8028e3242369f0b91f4f7199fdc63977
Douglas Berdeaux <weaknetlabs@gmail.com>
2018-12-21 12:39:07 -0500

    Added a few more excludes

--- fs-sync.sh
+++ fs-sync.sh
@@ -2,5 +2,5 @@
 # This is part of the new development process and should only be used for Debian 9+
 # New for 2019 Debian development
 # (C) GNU - WEAKNETLABS 2019, DEMON LINUX, Douglas Berdeaux
-WORKINGDIR= # This needs to be set in the root, so e.g: "/debianlive"
-rsync -a / $WORKINGDIR/debian-custom-iso-scripts/chroot --exclude=/{$WORKINGDIR,lib/live,live,cdrom,mnt,proc,run,sys,media,boot}
+WORKINGDIR=demon-dev # This needs to be set in the root, so e.g: "/debianlive"
+rsync -a / $WORKINGDIR/debian-custom-iso-scripts/chroot --exclude=/{$WORKINGDIR,lib/live,live,cdrom,mnt,proc,run,sys,media,boot,vmlinuz,vmlinuz.old,initrd.img,initrd.img.old,tmp,lost+found,srv}
