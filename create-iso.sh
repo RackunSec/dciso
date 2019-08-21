be7f3d8573f829099e42f84c23bd6e1e46e1d219
Douglas Berdeaux <weaknetlabs@gmail.com>
2019-08-21 10:56:47 -0400

    HUGE GRUB overhaul - long overdue, WIP.

--- create-iso.sh
+++ create-iso.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#
+# UPDATED for DEMON LINUX using 64 bit OS and GRUB
 # This script creates the ISO and SquashFS
 #  It will be a "hybrid" image that you can put onto USB using disk-damager
 #
@@ -13,62 +13,88 @@ myPrintf () { # cut back on code rewrite
  printf "$wc\xe2\x98\x85 $yc$1$nc\n";
 }
 
-# Set up working environment (./binary)
-myPrintf "Creating development directories in ./binary"
+###
+### Set up working environment (./binary)
+myPrintf "Creating development directories in ./binary ... "
 rm -rf binary # remove old dev files
-mkdir -p binary/{live,isolinux} # create a few directories
-myPrintf "Copying LINUX to the development directory" # copy over LINUX
-cp chroot/boot/vmlinuz-* binary/live/vmlinuz
-cp chroot/boot/initrd.img-* binary/live/initrd
-myPrintf "Making SquashFS filesystem, this will take some time."
+mkdir -p {image/live,scratch}
 
-# Create the Squash Filesystem
-mksquashfs chroot binary/live/filesystem.squashfs -comp xz -e boot
+###
+### Create SquashFS:
+myPrintf "Making SquashFS filesystem, this will take some time ... "
+#mksquashfs chroot image/live/filesystem.squashfs -comp xz -e boot
 
-# copy files from the chroot - 
-mkdir /usr/lib/ISOLINUX
-cp chroot/usr/lib/ISOLINUX/iso* /usr/lib/ISOLINUX/
+###
+### Copy the VMLINUZ and INITRD into the ./binary directory:
+myPrintf "Copying Kernel and InitRAMFS to the development directory ... " # copy over LINUX
+cp chroot/boot/vmlinuz-* image/vmlinuz
+cp chroot/boot/initrd.img-* image/initrd
 
-# Create the actual IS0
-myPrintf "Creating the ISO image, this will take some time."
-if [ -f "binary/live/filesystem.squashfs" ];then # filesystem successfully made
- # cp /usr/lib/ISOLINUX/isolinux.bin binary/isolinux/ # ISOLINUX for booting into live ISO
- cp /usr/share/live/build/bootloaders/isolinux/isolinux.bin binary/isolinux/
- cp /usr/lib/syslinux/modules/bios/menu.c32 binary/isolinux/
- cp /usr/lib/syslinux/modules/bios/hdt.c32 binary/isolinux/
- cp /usr/lib/syslinux/modules/bios/ldlinux.c32 binary/isolinux/
- cp /usr/lib/syslinux/modules/bios/libcom32.c32 binary/isolinux/
- cp /usr/lib/syslinux/modules/bios/libutil.c32 binary/isolinux/
- cp /usr/lib/syslinux/modules/bios/vesamenu.c32 binary/isolinux/ # for nice boot menu?
+### 
+### Copt GRUB Menu over to new image area:
+cp grub/grub.cfg scratch/grub.cfg
 
- # Create an ISOLinux menu:
- myPrintf "Creating the ISOLINUX configuration file" # this can be scripted to take args later:
- cp isolinux/isolinux.cfg binary/isolinux/ # ISOLINUX is now own directory
- TS=$(date|awk '{gsub(":",".",$4); print $3$4$6}')
- xorriso -as mkisofs -r -J -joliet-long -l -cache-inodes \
-  -isohybrid-mbr /usr/lib/ISOLINUX/isohdpfx.bin -partition_offset 16 \
-  -A "Debian Live"  -b isolinux/isolinux.bin -c \
-  isolinux/boot.cat -no-emul-boot -boot-load-size 4 \
-  -boot-info-table -o wt7-elite-$TS.iso binary
-else
- myPrintf "SquashFS not created. Something went wrong. Please see details above"
- exit 1;
-fi 
+touch image/DEMON_CUSTOM
 
-# Create an MD5 checksum
-myPrintf "Creating MD5 checksum for your ISO image."
-if [ -f "wt7-elite-$TS.iso" ]; then # file exists:
- myPrintf "ISO file wt7-elite-$TS.iso created successfully, generating MD5"
- md5sum wt7-elite-$TS.iso |awk '{print $1}' > wt7-elite-$TS.md5
-else # no ISO
- myPrintf "ISO was not created, something went wrong. Please see details above."
- exit 1;
-fi # now check for MD5 existence:
-if [ -f "wt7-elite-$TS.md5" ];then
- myPrintf "MD5 file wt7-elite-$TS.md5 created successfully."
-else
- myPrintf "MD5 file could not be created. Something went wrong. Please try with \"md5sum wt7-elite-$TS.iso\""
- exit 1;
-fi
-exit 0
+#################################
+### UEFI/BIOS Booting:
 
+### (step 1)
+### Create Grub UEFI/BIOS image:
+grub-mkstandalone \
+	--format=x86_64-efi \
+	--output=scratch/bootx64.efi \
+	--locales="en_US.UTF-8" \
+	--fonts="" \
+	"boot/grub/grub.cfg=scratch/grub.cfg"
+
+### (step 2)
+### Create Fat 16 UEFI boot disk image:
+(cd scratch && dd if=/dev/zero of=efiboot.img bs=1M count=10 && \
+	mkfs.vfat efiboot.img && \
+	mmd -i efiboot.img efi efi/boot && \
+	mcopy -i efiboot.img ./bootx64.efi ::efi/boot/)
+
+### (step 3)
+### Create GRUB BIOS Image:
+grub-mkstandalone \
+	--format=i386-pc \
+	--output=scratch/core.img \
+	--install-modules="linux normal iso9660 biosdisk memdisk search tar ls" \
+	--modules="linux normal iso9660 biosdisk search" \
+	--locales="en_US.UTF-8" \
+	--fonts="" \
+	"boot/grub/grub.cfg=scratch/grub.cfg"
+
+### (step 4)
+### Combine GRUB with our Bootloader IMG:
+cat /usr/lib/grub/i386-pc/cdboot.img \
+	scratch/core.img \
+	> scratch/bios.img
+
+###
+### Finally, Generate the ISO Image file:
+xorriso \
+	-as mfisofs \
+	-iso-level 3 \
+	-full-iso9660-filenames \
+	-volid "DEMON_CUSTOM" \
+	-eltorito-boot \
+	 boot/grub/bios.img \
+	 -no-emul-boot \
+	 -boot-load-size 4 \
+	 -boot-info-table \
+	 --eltorito-catalog boot/grub/boot.cat \
+	--grub2-boot-info \
+	--grub2-mbr /usr/lib/grub/i386-pc/boot_hybrid.img \
+	--eltorito-alt-boot \
+	 -e EFI/efiboot.img \
+	 -no-emul-boot \
+	 -append_partition 2 0xef scratch/efiboot.img \
+	 -output "demon_custom.iso" \
+	 -graft-points \
+	   "image" \
+	   /boot/grub/bios.img=scratch/bios.img \
+	   /EFI/efiboot.img=scratch/efiboot.img
+
+## I mean, what could go wrong ??
