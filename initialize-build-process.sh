a77dfbb8876112f645b1eafad86b495d0d1a814f
Douglas Berdeaux <weaknetlabs@gmail.com>
2019-12-20 08:46:55 -0500

    Updated script to require argument

--- initialize-build-process.sh
+++ initialize-build-process.sh
@@ -2,6 +2,11 @@
 # Specify the distribution code name (e.g.: buster, sid, stretch, etc)
 # This script will setup the chrooted env to begin customization and should ONLY be
 #  ran once at the beginning of the entire development process
+if [ "$1" == "" ]
+then
+ printf "Please give me an argument Debian version, e.g.: buster, squeeze, wheezy\n"
+ exit
+fi
 if [ -d "./chroot" ]; then
  printf "WARNING!! directory \"chroot\" found.\nIf you continue, all work in \"chroot\" will be LOST.\n\n";
  printf "Continue (y/n)? ";
